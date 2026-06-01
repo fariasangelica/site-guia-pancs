9d5a3136edd191ab4d17e7f8c80ce97d38d55f79
Angelica Farias  Oliveira <hendhiooo@gmail.com>
2026-06-01 09:18:02 -0300

    Remove admin password requirement from painel

--- src/app/painel/page.tsx
+++ src/app/painel/page.tsx
@@ -1,13 +1,10 @@
-import { cookies } from "next/headers";
 import { ContentShell } from "@/components/layout/ContentShell";
-import { AdminLoginForm } from "@/components/admin/AdminLoginForm";
 import { PanelPageClient } from "@/components/pages/PanelPageClient";
-import { ADMIN_COOKIE_NAME, isValidAdminSessionToken } from "@/lib/adminAuth";
 
 export default async function PainelPage() {
-  const cookieStore = await cookies();
-  const sessionToken = cookieStore.get(ADMIN_COOKIE_NAME)?.value;
-  const isAuthenticated = isValidAdminSessionToken(sessionToken);
-
-  return <ContentShell>{isAuthenticated ? <PanelPageClient /> : <AdminLoginForm />}</ContentShell>;
+  return (
+    <ContentShell>
+      <PanelPageClient />
+    </ContentShell>
+  );
 }
